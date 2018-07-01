a1e831d4e04bddfa0ff7f90ee20db4334f1f70e0
Neil <neil.pathare@outlook.com>
2018-06-30 19:14:14 -0500

    Updating Home for Theme & SideNav

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { ElectronService } from '../../providers/electron.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(public overlayContainer: OverlayContainer, public electronService: ElectronService) {}
+
+  @HostBinding('class') componentCssClass;
 
   ngOnInit() {}
+
+  onSetTheme(theme) {
+    this.overlayContainer.getContainerElement().classList.add(theme);
+    this.componentCssClass = theme;
+  }
+
+  minimizeApp() {
+    this.electronService.ipcRenderer.send('app-minimize', true);
+  }
+
+  maximizeApp() {
+    this.electronService.ipcRenderer.send('app-maximize', true);
+  }
+
+  closeApp() {
+    this.electronService.ipcRenderer.send('app-close', true);
+  }
 }
